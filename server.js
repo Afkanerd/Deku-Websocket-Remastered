f3dcad339954c093a3e6b8b12fc74655eb702828
Promise Fru <promisefru1@gmail.com>
2021-01-03 11:26:37 +0100

    setup

---
+++ server.js
@@ -0,0 +1,31 @@
+const net = require('net');
+const port = 3000;
+
+function createServerConnection() {
+    return new Promise((resolve, reject) => {
+        try {
+            let server = net.createServer();
+
+            server.listen(port, console.log('Server listening on port ' + port))
+            resolve(server)
+        } catch (err) {
+            reject(err)
+        }
+    })
+}
+
+// (async () => {
+//     try {
+//         let serverConnection = await createServerConnection();
+
+//         serverConnection.on('connection', (client) => {
+//             console.log('A client connected');
+
+//             client.on('end', () => {
+//                 console.log('A client disconnected')
+//             })
+//         })
+//     } catch (err) {
+//         throw err
+//     }
+// })();
