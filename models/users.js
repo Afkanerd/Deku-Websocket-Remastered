e4c39cf5d28c8835986df47200b199283a66f8b8
Promise Fru <promisefru1@gmail.com>
2021-01-04 06:57:39 +0100

    updated Users Model: create method

--- models/users.js
+++ models/users.js
@@ -1,4 +1,8 @@
-const Sequelize = require("sequelize");
+const {
+    Sequelize,
+    DataTypes,
+    UUIDV4
+} = require("sequelize");
 const cred = require("../credentials.json")
 
 // create connection : new Sequelize(database, username, password, {host, dialect})
@@ -7,3 +11,43 @@ const sequelize = new Sequelize(cred.DATABASE, cred.USERNAME, cred.PASSWORD, {
     dialect: "mysql"
 });
 
+const user = sequelize.define("User", {
+    uuid: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        primaryKey: true
+    },
+    name: DataTypes.STRING,
+    token: DataTypes.STRING
+})
+
+/**
+ * Class representing a sser
+ */
+class Users {
+    /**
+     * create a new user
+     * @param {STRING} name first and last name
+     * @param {STRING} token generated token
+     * @returns {object} {UUID, name}
+     */
+    create(name, token) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                await user.sync();
+                var newUser = await user.create({
+                    name: name,
+                    token: token
+                })
+                resolve({
+                    uuid: newUser.uuid,
+                    name: newUser.name
+                })
+            } catch (e) {
+                reject(e)
+            }
+        })
+    }
+}
+
+module.exports = Users
--- test/unit_test.js
+++ test/unit_test.js
@@ -3,7 +3,9 @@ const {
     Sequelize,
     QueryTypes
 } = require("sequelize");
-const cred = require("../credentials.json")
+const cred = require("../credentials.json");
+const Users = require("../models/users.js");
+var user = new Users();
 
 // create connection : new Sequelize(database, username, password, {host, dialect})
 const sequelize = new Sequelize(cred.DATABASE, cred.USERNAME, cred.PASSWORD, {
@@ -22,6 +24,24 @@ describe('Database Connection', () => {
 
     it("should connect", (done) => {
         assert.equal(result[0].sum, 2)
+        done();
+    })
+})
+
+describe('Users model', () => {
+    var newUser;
+    var name;
+    var token;
+
+    before(async () => {
+        name = "John Doe";
+        token = "11ee";
+        newUser = await user.create(name, token);
+    })
+
+    it("Create a new user", (done) => {
+        assert.property(newUser, "uuid");
+        assert.property(newUser, "name")
         done();
     })
 })
