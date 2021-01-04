873a8656d2b92f523085f4525392f0098163609c
Promise Fru <promisefru1@gmail.com>
2021-01-04 17:57:10 +0100

    sms model updated

--- test/unit_test.js
+++ test/unit_test.js
@@ -5,7 +5,9 @@ const {
 } = require("sequelize");
 const cred = require("../credentials.json");
 const Users = require("../models/users.js");
+const SMS = require('../models/sms.js');
 var user = new Users();
+var sms = new SMS();
 
 // create connection : new Sequelize(database, username, password, {host, dialect})
 const sequelize = new Sequelize(cred.DATABASE, cred.USERNAME, cred.PASSWORD, {
@@ -30,14 +32,14 @@ describe('Database Connection', () => {
 
 describe('Users model', () => {
     var newUser;
-    var name;
-    var token;
     var found;
+    var holder = {
+        name: "John Doe",
+        token: "11ee"
+    }
 
     before(async () => {
-        name = "John Doe";
-        token = "11ee";
-        newUser = await user.create(name, token);
+        newUser = await user.create(holder.name, holder.token);
         found = await user.find(newUser.uuid)
     })
 
@@ -53,4 +55,28 @@ describe('Users model', () => {
 
         done();
     })
+})
+
+describe('SMS model', () => {
+    var result;
+    var test = {
+        sender: "John Doe",
+        reciever: "Peter2 Head",
+        number: 670918263,
+        body: "hello world test sms"
+    }
+
+    before(async () => {
+        result = await sms.create(test.sender, test.reciever, test.number, test.body);
+    })
+
+    it("Create a new SMS", (done) => {
+        assert.property(result, "sender");
+        assert.property(result, "reciever");
+        assert.property(result, "number");
+        assert.property(result, "body");
+        assert.property(result, "status");
+        done();
+    })
+
 })
