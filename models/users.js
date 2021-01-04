b04d5972f30b63579af7555385949b10aeae1e87
Promise Fru <promisefru1@gmail.com>
2021-01-04 04:49:01 +0100

    test db connection

---
+++ models/users.js
@@ -0,0 +1,9 @@
+const Sequelize = require("sequelize");
+const cred = require("../credentials.json")
+
+// create connection : new Sequelize(database, username, password, {host, dialect})
+const sequelize = new Sequelize(cred.DATABASE, cred.USERNAME, cred.PASSWORD, {
+    host: cred.HOST,
+    dialect: "mysql"
+});
+
--- test/unit_test.js
+++ test/unit_test.js
@@ -1,8 +1,27 @@
-const assert = require("assert");
+const assert = require("chai").assert;
+const {
+    Sequelize,
+    QueryTypes
+} = require("sequelize");
+const cred = require("../credentials.json")
 
-describe('Test case', () => {
-    it("should fail", (done) => {
-        assert.fail();
+// create connection : new Sequelize(database, username, password, {host, dialect})
+const sequelize = new Sequelize(cred.DATABASE, cred.USERNAME, cred.PASSWORD, {
+    host: cred.HOST,
+    dialect: "mysql"
+});
+
+describe('Database Connection', () => {
+    var result
+
+    before(async () => {
+        result = await sequelize.query("SELECT 1+1 AS sum", {
+            type: QueryTypes.SELECT
+        });
+    })
+
+    it("should connect", (done) => {
+        assert.equal(result[0].sum, 2)
         done();
     })
 })
