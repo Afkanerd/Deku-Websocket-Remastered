4389422ee9843196eb3df72cf1b18163441d2c69
Promise Fru <promisefru1@gmail.com>
2021-01-20 02:58:20 +0100

    database test removed from user and sms unit test

--- test/sms.test.js
+++ test/sms.test.js
@@ -1,33 +1,8 @@
 const assert = require("chai").assert;
-const {
-    Sequelize,
-    QueryTypes
-} = require("sequelize");
-const cred = require("../credentials.json");
 const SMS = require('../models/sms.js');
 var sms = new SMS();
 
-// create connection : new Sequelize(database, username, password, {host, dialect})
-const sequelize = new Sequelize(cred.DATABASE, cred.USERNAME, cred.PASSWORD, {
-    host: cred.HOST,
-    dialect: "mysql"
-});
-
 describe('SMS unit test', () => {
-    describe('Database Connection', () => {
-        var result
-
-        before(async () => {
-            result = await sequelize.query("SELECT 1+1 AS sum", {
-                type: QueryTypes.SELECT
-            });
-        })
-
-        it("should connect", (done) => {
-            assert.equal(result[0].sum, 2)
-            done();
-        })
-    })
 
     describe('SMS model', () => {
         var result;
--- test/user.test.js
+++ test/user.test.js
@@ -1,33 +1,8 @@
 const assert = require("chai").assert;
-const {
-    Sequelize,
-    QueryTypes
-} = require("sequelize");
-const cred = require("../credentials.json");
 const Users = require("../models/users.js");
 var user = new Users();
 
-// create connection : new Sequelize(database, username, password, {host, dialect})
-const sequelize = new Sequelize(cred.DATABASE, cred.USERNAME, cred.PASSWORD, {
-    host: cred.HOST,
-    dialect: "mysql"
-});
-
 describe('User Unit test', () => {
-    describe('Database Connection', () => {
-        var result
-
-        before(async () => {
-            result = await sequelize.query("SELECT 1+1 AS sum", {
-                type: QueryTypes.SELECT
-            });
-        })
-
-        it("should connect", (done) => {
-            assert.equal(result[0].sum, 2)
-            done();
-        })
-    })
 
     describe('Users model', () => {
         var newUser;
